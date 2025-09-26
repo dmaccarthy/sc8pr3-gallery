316681c1d918b2a7beec2e5a0abd9b8f56b5fb35
D. MacCarthy <sc8pr.py@gmail.com>
2025-09-26 15:09:33 -0600

    Update main.py to set CWD

--- examples/main.py
+++ examples/main.py
@@ -15,6 +15,11 @@
 # You should have received a copy of the GNU General Public License
 # along with "sc8pr3-gallery".  If not, see <http://www.gnu.org/licenses/>.
 
+
+# Ensure main.py is in current directory...
+import os
+os.chdir(os.path.split(__file__)[0])
+
 from sc8pr import Sketch, Image, LEFT, BOTTOM
 from sc8pr.sprite import Sprite
 from sc8pr.text import Text, Font
